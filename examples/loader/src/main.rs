4f3df7535b4c9a777864a73d1a87fb5ad3466ab5
“yumu20030130” <183608563@qq.com>
2024-12-03 16:59:49 +0800

    exercise 1 and 2

--- examples/loader/src/main.rs
+++ examples/loader/src/main.rs
@@ -5,16 +5,31 @@
 use axstd::println;
 
 const PLASH_START: usize = 0xffff_ffc0_2200_0000;
-
+const APP_NUM: usize = 2;
+const BATCH_SIZE: usize = 1024 * 1024; // 1MB 暂不支持大于1MB的应用
+pub struct ImageHeader {
+    files_size: [u8; APP_NUM],
+}
+// 第一个块固定用来存ImageHeader，后面一个块存放一个应用
+// 要配合对应指令把应用写到对应的位置，才能正常运行
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    let apps_start = PLASH_START as *const u8;
-    let apps_size = 32; // Dangerous!!! We need to get accurate size of apps.
+
+    let image_header = unsafe { core::slice::from_raw_parts(PLASH_START as *const u8, APP_NUM)};
+    let image_header = image_header.as_ptr() as *const ImageHeader;
+
+    let apps_start = PLASH_START + BATCH_SIZE;
 
     println!("Load payload ...");
-
-    let code = unsafe { core::slice::from_raw_parts(apps_start, apps_size) };
-    println!("content: {:?}: ", code);
+    let mut cur_offset = apps_start;
+    for i in 0..APP_NUM {
+        let file_size = unsafe { (*image_header).files_size[i] as usize };
+        let code = unsafe { core::slice::from_raw_parts(cur_offset as *const u8, file_size)};
+        cur_offset += BATCH_SIZE;
+        println!("app index: {:?}: ", i);
+        println!("app size: {:?}: ", file_size);
+        println!("content: {:?}: ", code);
+    }
 
     println!("Load payload ok!");
 }
